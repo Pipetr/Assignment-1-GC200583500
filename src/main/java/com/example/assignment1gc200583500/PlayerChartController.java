65ec18e6ee94ca3b0add972fb734818bf8c97e2b
Pipetr <faaa1901@gmail.com>
2025-06-20 21:32:26 -0400

    added view for player stats table view and controller

---
+++ src/main/java/com/example/assignment1gc200583500/PlayerChartController.java
@@ -0,0 +1,64 @@
+package com.example.assignment1gc200583500;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class PlayerChartController {
+
+    @FXML
+    private TableColumn<?, ?> assistsCol;
+
+    @FXML
+    private TableColumn<?, ?> blocksCol;
+
+    @FXML
+    private Button btnChart;
+
+    @FXML
+    private TableColumn<?, ?> countryCol;
+
+    @FXML
+    private TableColumn<?, ?> dreboundsCol;
+
+    @FXML
+    private TableColumn<?, ?> foulsCol;
+
+    @FXML
+    private TableColumn<?, ?> minutesCol;
+
+    @FXML
+    private TableColumn<?, ?> oreboundsCol;
+
+    @FXML
+    private TableColumn<?, ?> payerNameCol;
+
+    @FXML
+    private TableColumn<?, ?> pointsCol;
+
+    @FXML
+    private TableColumn<?, ?> positionCol;
+
+    @FXML
+    private TableColumn<?, ?> reboundsCol;
+
+    @FXML
+    private TableColumn<?, ?> stealsCol;
+
+    @FXML
+    private TableView<?> tableView;
+
+    @FXML
+    private TableColumn<?, ?> teamCol;
+
+    @FXML
+    private TableColumn<?, ?> turnoversCol;
+
+    @FXML
+    void chartView(ActionEvent event) {
+
+    }
+
+}
